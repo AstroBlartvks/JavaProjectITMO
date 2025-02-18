a67100be1f7d792a58436049cf7531e455389756
unknown <astroblartvks@gmail.com>
2025-02-18 21:02:42 +0300

    Тест новой ветки

--- src/main/java/org/AstrosLab/Main.java
+++ src/main/java/org/AstrosLab/Main.java
@@ -5,3 +5,15 @@ public class Main {
         System.out.println("Hello, World!");
     }
 }
+
+//Это юзать, а не нижнее
+//git checkout -b "developBanch"
+//git add .
+//git commit -m "Что-то внутри"
+//git push origin developBanch
+
+
+//git add .
+//git commit -m "Что-то внутри"
+//git remote add origin https://github.com/AstroBlartvks/JavaProjectITMO.git
+//git push -f origin master
