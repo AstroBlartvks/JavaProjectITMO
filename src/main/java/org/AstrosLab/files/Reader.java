117f9cf7af7f366271547e11736a24fccaf38cd0
unknown <astroblartvks@gmail.com>
2025-02-19 20:55:17 +0300

    Починил windows moment

--- src/main/java/org/AstrosLab/files/Reader.java
+++ src/main/java/org/AstrosLab/files/Reader.java
@@ -12,10 +12,7 @@ public class Reader {
 
     public Route readFromEnv() {
         String Path = System.getenv("JAVATESTFILE");
-
-        //Windows moment
-        Path = "C:\\Users\\AstroBlartvks\\IdeaProjects\\JavaProjectITMO\\src\\test\\collection.json";
-
+        
         if (Path == null){
             System.err.println("Путь к файлу в переменной окружения JAVATESTFILE не найден!");
             return null;
